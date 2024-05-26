ee950bdb2502048cd107ccbe08a59409c20e040f
captaincoro11 <captaincoro444@gmail.com>
2024-05-26 16:13:43 +0530

    ls

---
+++ routes/task.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express.Router();
+const Task = require('../model/task')
+
+app.get('/data',async(req,res)=>{
+    const tasks = await Task.find({});
+    
+    res.status(200).json({
+        message:"Tasks fetched successfully",
+        tasks
+    })
+})
